69404b4a054a36bbc479853c92599f97dfadefd1
Eric Bulloch <ebulloch.os@gmail.com>
2014-11-10 13:29:09 -0700

    If the message coming to the server is blank don't print anything

--- server.sh
+++ server.sh
@@ -5,5 +5,8 @@ while true; do
     # notify-send [OPTION...] <SUMMARY> [BODY] - create a notification
     # -t Specifies the timeout in milliseconds at which to expire the notification.
     # -a Specifies the app name for the icon
+    if [ -z "$line" ]; then
+        continue
+    fi
     notify-send -t 1000 -a "Alerty" "Status Report" "$line"
 done
